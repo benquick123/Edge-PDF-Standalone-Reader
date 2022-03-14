7db3dec3c8bc5f5221673246a858443d2cf22770
Benjamin Fele <benjamin.fele@gmail.com>
2022-03-14 18:48:05 +0100

    Update msedge.bat

--- msedge.bat
+++ msedge.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFDFbQgWWNGq0OpoS7czp5vyCnmkYR+krd5/n16eLJOcW1RW0JMVj33lV+A==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSTk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
-::ZQ05rAF9IAHYFVzEqQIVLBxRDDSmGiuIA7QY7fqb
-::eg0/rx1wNQPfEVWB+kM9LVsJDAmROW+9A4oM7O616vKCwg==
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFDFbQgWWNGq0OpoS7czp5vyCnmkYR+krd5/n16eLJOcW1VXgc9gozn86
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 echo off
 
 "%~dp0\msedge.exe" --new_window --app=%1
